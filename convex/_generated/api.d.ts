c7b1640f62e8b5c44e10429f0f6b0a63775767e6
youssefmd2244-droid <youssefmd2244@gmail.com>
2026-05-14 06:43:47 +0300

    api.d.ts
    إضافة ملفات _generated

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,1 +1,67 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
 
+import type * as ViktorSpacesEmail from "../ViktorSpacesEmail.js";
+import type * as auth from "../auth.js";
+import type * as constants from "../constants.js";
+import type * as generations from "../generations.js";
+import type * as http from "../http.js";
+import type * as seedTestUser from "../seedTestUser.js";
+import type * as social from "../social.js";
+import type * as testAuth from "../testAuth.js";
+import type * as users from "../users.js";
+import type * as viktorTools from "../viktorTools.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  ViktorSpacesEmail: typeof ViktorSpacesEmail;
+  auth: typeof auth;
+  constants: typeof constants;
+  generations: typeof generations;
+  http: typeof http;
+  seedTestUser: typeof seedTestUser;
+  social: typeof social;
+  testAuth: typeof testAuth;
+  users: typeof users;
+  viktorTools: typeof viktorTools;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
